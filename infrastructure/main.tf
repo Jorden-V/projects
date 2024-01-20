57f314fd377c6b8ff6c1b9aa77872f613351e5a0
Jorden <jordenkv@gmail.com>
2024-01-20 22:41:09 +0000

    Test remove access token

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,17 +9,17 @@ data "google_project" "current" {
   project_id = var.project
 }
 
-data "google_service_account_access_token" "default" {
-  provider               = google.token
-  target_service_account = "terraform@${var.project}.iam.gserviceaccount.com"
-  scopes                 = ["cloud-platform"]
-  lifetime               = "1200s"
-}
+# data "google_service_account_access_token" "default" {
+#   provider               = google.token
+#   target_service_account = "terraform@${var.project}.iam.gserviceaccount.com"
+#   scopes                 = ["cloud-platform"]
+#   lifetime               = "1200s"
+# }
 
 provider "google" {
-  project      = var.project
-  region       = var.region
-  access_token = data.google_service_account_access_token.default.access_token
+  project = var.project
+  region  = var.region
+  # access_token = data.google_service_account_access_token.default.access_token
 }
 
 locals {
